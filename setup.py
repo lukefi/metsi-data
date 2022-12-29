5323a61259861a653eca1b57cd97db514b26c05f
JP Yliuntinen <83706030+jpy-luke@users.noreply.github.com>
2022-12-29 16:19:58 +0200

    chore: bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name="forestdatamodel",
     description="Data classes and utilities for forest stand, tree strata and reference trees representation",
-    version="0.4.7",
+    version="1.0.0-rc1",
     packages=setuptools.find_namespace_packages(include=['forestdatamodel*']),
     install_requires=[
         "geopandas==0.12.*",
